7536937ec76c62756dbf6887eb94df6f76262220
Tobias Fuchs <tobias.fuchs@kinexon.com>
2023-08-01 16:59:56 +0200

    add python and zettelkasten

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,6 +12,8 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.coding.yanky" },
+    { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
