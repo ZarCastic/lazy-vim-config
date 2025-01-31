f65c99f9dbf127edc1f8666754d48923ab8585a2
Tobias Fuchs <tobias.fuchst@tfox.tech>
2025-01-31 20:22:36 +0100

    fix python config

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -91,19 +91,6 @@ return {
     },
   },
 
-  -- Setup null-ls with `black`
-  {
-    "nvimtools/none-ls.nvim",
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      opts.sources = vim.list_extend(opts.sources, {
-        -- Order of formatters matters. They are used in order of appearance.
-        nls.builtins.formatting.ruff,
-        nls.builtins.formatting.black,
-      })
-    end,
-  },
-
   -- For selecting virtual envs
   {
     "linux-cultist/venv-selector.nvim",
